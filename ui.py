59c5b712db166b84d93b282080eb55110e4b25ab
UnityTheCoder <unitythecodergit@gmail.com>
2022-10-01 19:03:39 +0200

    New Push

--- ui.py
+++ ui.py
@@ -42,35 +42,54 @@ vline="┃"
 hline="━"
 
 #   ╭  ╮    ╰  ╯
- 
 
-info = ["^d", "&dz", "l09dfdessd38uhuhffd", "83d"]
-lens = []
-for inf in info:
-    lens.append(len(inf))
 
 most = max(lens)
 # print(most)
+def retrieveL(dlen, end):
+    return " " * ((end - dlen) - 1)
 
-distro=info[0]
-cpu=info[1]
-ram=info[2]
 
-end = len(f"╭━━━━━━━━━━━━━━━━━━{hline * most}╮")
-distrolen = len(f"┃   DISTRO:    {distro}")
-cpulen = len(f"┃      CPU:    {cpu}")
-ramlen = len(f"┃      RAM:    {ram}")
+def menu(info):
+    lens = []
+    for inf in info:
+        lens.append(len(inf))
 
-newdilen = " " * ((end - distrolen) - 1)
-newcplen = " " * ((end - cpulen) - 1)
-newrmlen = " " * ((end - ramlen) - 1)
+    most = max(lens)
 
-menu = f"""
-╭━━━━━━━━━━━━━━━━━━{hline * most}╮
-┃   DISTRO:    {distro}{newdilen}┃
-┃      CPU:    {cpu}{newcplen}┃
-┃      RAM:    {ram}{newrmlen}┃
-╰━━━━━━━━━━━━━━━━━━{hline * most}╯
-"""
+    distro=info[0]
+    cpu=info[1]
+    ram=info[2]
 
 print(menu)
+=======
+    end = len(f"╭━━━━━━━━━━━━━━━━━━{hline * most}╮")
+    distrolen = len(f"┃   DISTRO:    {distro}")
+    cpulen = len(f"┃      CPU:    {cpu}")
+    ramlen = len(f"┃      RAM:    {ram}")
+
+    newdilen = retrieveL(distrolen, end)
+    newcplen = retrieveL(cpulen, end)
+    newrmlen = retrieveL(ramlen, end)
+
+
+    spacing = " " * 4
+    fields = [
+        f"{Fore.YELLOW}{Style.BRIGHT}DISTRO:{spacing}{distro}{newdilen}{Style.NORMAL}{Fore.RESET}",
+        f"   CPU:{spacing}{cpu}{newcplen}",
+        f"   RAM:{spacing}{ram}{newrmlen}",
+    ]
+
+    menu = f"""
+╭━━━━━━━━━━━━━━━━━━{hline * most}╮\n"""
+    for field in fields:
+        menu+=f"┃   {field}┃\n"
+
+
+    menu+=f"╰━━━━━━━━━━━━━━━━━━{hline * most}╯\n"
+
+    print(menu)
+
+
+info=["d4", "3s", "5m", "4s"]
+menu(info)
