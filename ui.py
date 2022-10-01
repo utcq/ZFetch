74de4dc87191097ad4b5473f0699222839c84b83
UnityTheCoder <unitythecodergit@gmail.com>
2022-10-01 15:41:12 +0200

    New Push

--- ui.py
+++ ui.py
@@ -1,4 +1,4 @@
-
+from colorama import *
 
 class DistroIcons:
     linux=""
@@ -37,4 +37,41 @@ class DistroIcons:
     void=""
     zorin=""
     windows=""
+
+vline="┃"
+hline="━"
+
+#   ╭  ╮    ╰  ╯
+ 
+
+info = ["^d", "&dz", "l09dfdessd38uhuhffd", "83d"]
+lens = []
+for inf in info:
+    lens.append(len(inf))
+
+most = max(lens)
+print(most)
+
+distro=info[0]
+cpu=info[1]
+ram=info[2]
+
+end = len(f"╭━━━━━━━━━━━━━━━━━━{hline * most}╮")
+distrolen = len(f"┃   DISTRO:    {distro}")
+cpulen = len(f"┃      CPU:    {cpu}")
+ramlen = len(f"┃      RAM:    {ram}")
+
+newdilen = " " * ((end - distrolen) - 1)
+newcplen = " " * ((end - cpulen) - 1)
+newrmlen = " " * ((end - ramlen) - 1)
+
+menu = f"""
+╭━━━━━━━━━━━━━━━━━━{hline * most}╮
+┃   DISTRO:    {distro}{newdilen}┃
+┃      CPU:    {cpu}{newcplen}┃
+┃      RAM:    {ram}{newrmlen}┃
+╰━━━━━━━━━━━━━━━━━━{hline * most}╯
+"""
+
+print(menu)
 
