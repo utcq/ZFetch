d3ee13fc1782af0ba9a509d04b214236051c53dc
UnityTheCoder <unitythecodergit@gmail.com>
2022-10-01 19:48:27 +0200

    Added entries on menu

--- ui.py
+++ ui.py
@@ -42,39 +42,73 @@ class DistroIcons:
 vline="┃"
 hline="━"
 
-#   ╭  ╮    ╰  ╯
 
 
+#   get spacing between info and menu end
 def retrieveL(dlen, end):
     return " " * ((end - dlen) - 1)
 
-
+# shows menu
 def menu(info):
+    #get largest info
     lens = []
     for inf in info:
         lens.append(len(inf))
 
     most = max(lens)
 
+    
+    #get info variables
+
     distro=info[0]
     cpu=info[1]
     ram=info[2]
-
+    gpu=info[3]
+    wm=info[4]
+    pkgs=info[5]
+    uptime=info[6]
+    kernel=info[7]
+    
+    
     end = len(f"╭━━━━━━━━━━━━━━━━━━{hline * most}╮")
     distrolen = len(f"┃   DISTRO:    {distro}")
     cpulen = len(f"┃      CPU:    {cpu}")
     ramlen = len(f"┃      RAM:    {ram}")
+    gpulen = len(f"┃      GPU:    {gpu}")
+    wmlen = len(f"┃    WM/DE:    {wm}")
+    pkgslen = len(f"┃     PKGS:    {pkgs}")
+    uptimelen = len(f"┃   UPTIME:    {uptime}")
+    kernellen = len(f"┃   KERNEL:    {kernel}")
+
+
+
+
+
 
     newdilen = retrieveL(distrolen, end)
     newcplen = retrieveL(cpulen, end)
     newrmlen = retrieveL(ramlen, end)
+    newgplen = retrieveL(gpulen, end)
+    newwmlen = retrieveL(wmlen, end)
+    newpklen = retrieveL(pkgslen, end)
+    newuplen = retrieveL(uptimelen, end)
+    newkelen = retrieveL(kernellen, end)
 
 
     spacing = " " * 4
+
+    #               To Update:      Replace DISTRO:                               Replace distro      Replace new*len
     fields = [
-        f"{Fore.YELLOW}{Style.BRIGHT}DISTRO:{Style.RESET_ALL}{Fore.BLUE}{spacing}{distro}{newdilen}{Style.RESET_ALL}{Fore.RESET}",
-        f"   CPU:{spacing}{cpu}{newcplen}",
-        f"   RAM:{spacing}{ram}{newrmlen}",
+        f"{Fore.YELLOW}{Style.BRIGHT}\033[3mDISTRO:{Style.RESET_ALL}\033[0m{Fore.BLUE}{spacing}{distro}{newdilen}{Style.RESET_ALL}{Fore.RESET}",
+        f"{Fore.YELLOW}{Style.BRIGHT}\033[3m   CPU:{Style.RESET_ALL}\033[0m{Fore.BLUE}{spacing}{cpu}{newcplen}{Style.RESET_ALL}{Fore.RESET}",
+        f"{Fore.YELLOW}{Style.BRIGHT}\033[3m   RAM:{Style.RESET_ALL}\033[0m{Fore.BLUE}{spacing}{ram}{newrmlen}{Style.RESET_ALL}{Fore.RESET}",
+        f"{Fore.YELLOW}{Style.BRIGHT}\033[3m   GPU:{Style.RESET_ALL}\033[0m{Fore.BLUE}{spacing}{gpu}{newgplen}{Style.RESET_ALL}{Fore.RESET}",
+        f"{Fore.YELLOW}{Style.BRIGHT}\033[3m WM/DE:{Style.RESET_ALL}\033[0m{Fore.BLUE}{spacing}{wm}{newwmlen}{Style.RESET_ALL}{Fore.RESET}",
+        f"{Fore.YELLOW}{Style.BRIGHT}\033[3m  PKGS:{Style.RESET_ALL}\033[0m{Fore.BLUE}{spacing}{pkgs}{newpklen}{Style.RESET_ALL}{Fore.RESET}",
+        f"{Fore.YELLOW}{Style.BRIGHT}\033[3mUPTIME:{Style.RESET_ALL}\033[0m{Fore.BLUE}{spacing}{uptime}{newuplen}{Style.RESET_ALL}{Fore.RESET}",
+        f"{Fore.YELLOW}{Style.BRIGHT}\033[3mKERNEL:{Style.RESET_ALL}\033[0m{Fore.BLUE}{spacing}{kernel}{newkelen}{Style.RESET_ALL}{Fore.RESET}",
+
+
     ]
 
     menu = f"""
@@ -88,5 +122,5 @@ def menu(info):
     print(menu)
 
 
-info=["d4", "3s", "5m", "4s"]
+info=["Gentoo", "Threadripper 5000WX 5.2GHz", "128GB", "Nvidia RTX 4090", "DWM", "2671", "5 Hours, 28 mins", "5.19.7-zen2-1-zen"]
 menu(info)
